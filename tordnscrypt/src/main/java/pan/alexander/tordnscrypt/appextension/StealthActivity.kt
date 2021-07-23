e8acf04d036ad7a6734a7bc46a174060183b0c31
yuliabatova <Yulia.batova@fulldive.com>
2021-07-23 10:10:12 +0300

    [5282] StealthActivity

---
+++ tordnscrypt/src/main/java/pan/alexander/tordnscrypt/appextension/StealthActivity.kt
@@ -0,0 +1,11 @@
+package pan.alexander.tordnscrypt.appextension
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StealthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
